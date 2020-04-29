2b3e6adcf1f59276eb2836edcf0f752296329b8d
Gratian <grati_marian@yahoo.com>
2020-04-29 15:17:41 +0200

    Telesillas / Nº: 419

---
+++ AceptaElReto/java/400-499/Telesillas419.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Telesillas419 {
+    public static void main(String[] args) {
+        final Scanner s = new Scanner(System.in);
+
+        int max, many, res, left, right;
+        int[] personas;
+
+        while (true) {
+
+            max  = s.nextInt();
+            many = s.nextInt();
+            if (max == 0 && many == 0) break;
+
+            personas = new int[many];
+
+            for (int i = 0; i < many; i++)
+                personas[i] = s.nextInt();
+
+            Arrays.sort( personas );
+
+            res = left = 0;
+            right = many - 1;
+
+            while (left <= right) {
+                if (left == right) { res++; left++; right--;}
+                else if (personas[left] + personas[right] <= max) {
+                    right--; left++;
+                    res++;
+                }
+                else {
+                    res++;
+                    right--;
+                }
+            }
+
+            System.out.println( res );
+        }
+    }
+}
